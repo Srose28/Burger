872fe5416b14a5468205145714f4d426fc5b982f
Rose, Simone <simoneidance@icloud.com>
2019-10-05 19:36:27 -0500

    Burger App Draft

---
+++ config/orm.js
@@ -0,0 +1,27 @@
+var connection = require('./connection.js');
+
+var orm = {
+    
+  selectAll: function(tableData, cb){
+    connection.query('SELECT * FROM ' + tableData + ';', function(err,result){
+      if(err) throw err;
+      cb(result);
+    });
+  },
+  
+  updateSetWhere: function(tableData, condition, cb){
+    connection.query('UPDATE ' + tableData +' SET devoured=true WHERE id=' + condition + ';', function(err,result){
+      if(err)throw err;
+      cb(result);
+    });
+  },
+  
+  insertIntoValues: function(tableData, val, cb){
+    connection.query('INSERT INTO ' + tableData + " (burger_name) VALUES ('"+val+"');", function(err,result){
+      if(err)throw err;
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
